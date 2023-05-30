f62a78cabfadcd75f545e655d0a0a81db1b7f0f3
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-05-30 22:35:09 +0200

    print on page for guest data

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $comics = Comic::all();
+
+        return view('welcome', compact('comics'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,21 @@
 
 @section('content')
 layout coming soon...🙄
+
+<ul>
+    @forelse($comics as $comic)
+    <li>
+        {{ $comic->title }} <br>
+        {{ $comic->description }} <br>
+        <img src="{{ $comic->thumb }}" alt=""> <br>
+        {{ $comic->price }} <br>
+        {{ $comic->series }} <br>
+        {{ $comic->sale_date }} <br>
+        {{ $comic->type }} <br>
+    </li>
+    @empty
+    <p>sorry we don't have comic for you 😝</p>
+    @endforelse
+</ul>
+
 @endsection
