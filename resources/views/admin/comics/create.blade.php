a610035ea3ad3f02989b78480734915e9dea1f45
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-06-01 15:25:10 +0200

    add error directive and value old

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -7,36 +7,36 @@
 
         <div class="mb-3">
           <label for="title" class="form-label fs-4 fw-bold">Title</label>
-          <input type="text" name="title" id="title" class="form-control" placeholder="" aria-describedby="helpId" required>
+          <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="" aria-describedby="helpId" value="{{ old('title') }}" required>
           <small id="helpId" class="text-muted">Add title</small>
         </div>
 
         <div class="mb-3">
           <label for="thumb" class="form-label fs-4 fw-bold">Image</label>
-          <input type="text" name="thumb" id="thumb" class="form-control" placeholder="" aria-describedby="helpId" required>
+          <input type="text" name="thumb" id="thumb" class="form-control @error('thumb') is-invalid @enderror" placeholder="" aria-describedby="helpId" value="{{ old('thumb') }}" required>
           <small id="helpId" class="text-muted">Add image path</small>
         </div>
 
         <div class="mb-3">
           <label for="description" class="form-label fs-4 fw-bold">Description</label>
-          <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+          <textarea class="form-control @error('title') is-invalid @enderror" name="description" id="description" rows="3">{{ old('description') }}" </textarea>
         </div>
 
         <div class="mb-3">
           <label for="price" class="form-label fs-4 fw-bold">Price</label>
-          <input type="text" name="price" id="price" class="form-control" placeholder="" aria-describedby="helpId" required>
+          <input type="text" name="price" id="price" class="form-control @error('price') is-invalid @enderror" placeholder="" aria-describedby="helpId" value="{{ old('price') }}" required>
           <small id="helpId" class="text-muted">Add price</small>
         </div>
 
         <div class="mb-3">
           <label for="sale_date" class="form-label fs-4 fw-bold">Sale Date</label>
-          <input type="date" name="sale_date" id="sale_date" class="form-control" placeholder="" aria-describedby="helpId">
+          <input type="date" name="sale_date" id="sale_date" class="form-control @error('sale_date') is-invalid @enderror" placeholder="" aria-describedby="helpId" value="{{ old('sale_date') }}" >
           <small id="helpId" class="text-muted">Add sale date </small>
         </div>
 
         <div class="mb-3">
           <label for="type" class="form-label fs-4 fw-bold">Type</label>
-          <input type="text" name="type" id="type" class="form-control" placeholder="" aria-describedby="helpId">
+          <input type="text" name="type" id="type" class="form-control @error('type') is-invalid @enderror" placeholder="" aria-describedby="helpId" value="{{ old('type') }}" >
           <small id="helpId" class="text-muted">Add kind of book</small>
         </div>
 
--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container py-5 bg-dark-subtle rounded-4 my-4">
 
     <div class="text-center my-3">
         <h6><strong>Editing: </strong> {{$comic->title}} </h6>
@@ -14,37 +14,38 @@
 
         <div class="mb-3">
             <label for="title" class="form-label fs-4 fw-bold">Title</label>
-            <input type="text" name="title" id="title" class="form-control" placeholder="Zelda" aria-describedby="nameHelper" value="{{$comic->title}}" required>
+            <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" aria-describedby="nameHelper" value="{{ old('price', $comic->title) }}" required>
             <small id="nameHelper" class="text-muted">Change Title</small>
         </div>
 
         <div class="mb-3">
             <label for="thumb" class="form-label fs-4 fw-bold">Image</label>
-            <input type="text" name="thumb" id="thumb" class="form-control" placeholder="https://" aria-describedby="thumbHelper" value="{{$comic->thumb}}" required>
+            <input type="text" name="thumb" id="thumb" class="form-control @error('thumb') is-invalid @enderror" aria-describedby="thumbHelper" value="{{ old('price', $comic->thumb) }}" required>
             <small id="thumbHelper" class="text-muted">Change Image Path</small>
         </div>
 
         <div class="mb-3">
-            <label for="description" class="form-label fs-4 fw-bold">Description</label>
-            <input type="text" name="description" id="description" class="form-control" placeholder="Nintendo Switch" aria-describedby="descriptionHelper" value="{{$comic->description}}">
-            <small id="descriptionHelper" class="text-muted">Change Desciption</small>
+          <label for="description" class="form-label fs-4 fw-bold">Description</label>
+          <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3">{{old('price', $comic->description)}}
+          </textarea>
+          <small id="descriptionHelper" class="text-muted">Change Desciption</small>
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label fs-4 fw-bold">Price</label>
-            <input type="text" step="0.01" name="price" id="price" class="form-control" placeholder="99.99" aria-describedby="priceHelper" value="{{ old('price', $comic->price) }}" required>
+            <input type="text" step="0.01" name="price" id="price" class="form-control @error('price') is-invalid @enderror" aria-describedby="priceHelper" value="{{ old('price', $comic->price) }}" required>
             <small id="priceHelper" class="text-muted">Change Price</small>
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label fs-4 fw-bold">Sale Date</label>
-            <input type="date" name="sale_date" id="sale_date" class="form-control" placeholder="Zelda" aria-describedby="sale_dateHelper" value="{{$comic->sale_date}}">
+            <input type="date" name="sale_date" id="sale_date" class="form-control @error('sale_date') is-invalid @enderror" aria-describedby="sale_dateHelper" value="{{ old('price', $comic->sale_date) }}">
             <small id="sale_dateHelper" class="text-muted">Change Sale Date</small>
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label fs-4 fw-bold">Type</label>
-            <input type="text" name="type" id="type" class="form-control" placeholder="Zelda" aria-describedby="typeHelper" value="{{$comic->type}}">
+            <input type="text" name="type" id="type" class="form-control @error('type') is-invalid @enderror" aria-describedby="typeHelper" value="{{ old('price', $comic->type) }}">
             <small id="typeHelper" class="text-muted">Change Type</small>
         </div>
 
