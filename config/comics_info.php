3692e83994a7aa1860cb4d2318a256640aa7935a
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-06-02 21:12:13 +0200

    add db comics for general info

---
+++ config/comics_info.php
@@ -0,0 +1,69 @@
+<?php 
+
+return [
+    "pages" => [
+        "characters",
+        "comics",
+        "movies",
+        "tv",
+        "games",
+        "collectibles",
+        "videos",
+        "fans",
+        "news",
+        "shop",
+    ],
+
+    "dcComics" => [
+        "Characters",
+        "Comics",
+        "Movies",
+        "TV",
+        "Games",
+        "Videos",
+        "News",
+    ],
+
+    "shop" => [
+        "Shop DC",
+        "Shop DC Collectibles",
+    ],
+
+    "dc" => [
+        "Terms Of Use",
+        "Privacy policy (New)",
+        "Ad Choices",
+        "Advertising",
+        "Jobs",
+        "Subscriptions",
+        "Talent Workshops",
+        "CPSC Certificates",
+        "Ratings",
+        "Shop Help",
+        "Contact Us",
+    ],
+
+    "sites" => [
+        "DC",
+        "MAD Magazine",
+        "DC Kids",
+        "DC Universe",
+        "DC Power Visa",
+    ],
+
+    "socials" => [
+        "footer-facebook.png",
+        "footer-twitter.png",
+        "footer-youtube.png",
+        "footer-pinterest.png",
+        "footer-periscope.png",
+    ],
+
+    "info" => [
+        "digital comics" => "buy-comics-digital-comics.png",
+        "dc merchandise" => "buy-comics-merchandise.png",
+        "subscription" => "buy-comics-subscriptions.png",
+        "comic shop locator" => "buy-comics-shop-locator.png",
+        "dc power visa" => "buy-dc-power-visa.svg",
+      ]
+];
